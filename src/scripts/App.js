2fd3bfb6f17acb95aca4f355e232babffa639702
Arnaud Mesureur <arnaud.mesureur@gmail.com>
2020-01-05 18:59:17 +1100

    Build and commit dist folder

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,75 +1,76 @@
-import WebGLView from './webgl/WebGLView';
-import GUIView from './gui/GUIView';
+import WebGLView from "./webgl/WebGLView";
+import GUIView from "./gui/GUIView";
 
 export default class App {
+  constructor() {}
 
-	constructor() {
+  init() {
+    this.initWebGL();
+    this.initGUI();
+    this.addListeners();
+    this.animate();
+    this.resize();
+  }
 
-	}
+  initWebGL() {
+    this.webgl = new WebGLView(this);
+    document
+      .querySelector(".container")
+      .appendChild(this.webgl.renderer.domElement);
+  }
 
-	init() {
-		this.initWebGL();
-		this.initGUI();
-		this.addListeners();
-		this.animate();
-		this.resize();
-	}
+  initGUI() {
+    this.gui = new GUIView(this);
+  }
 
-	initWebGL() {
-		this.webgl = new WebGLView(this);
-		document.querySelector('.container').appendChild(this.webgl.renderer.domElement);
-	}
+  addListeners() {
+    this.handlerAnimate = this.animate.bind(this);
 
-	initGUI() {
-		this.gui = new GUIView(this);
-	}
+    window.addEventListener("resize", this.resize.bind(this));
+    window.addEventListener("keyup", this.keyup.bind(this));
 
-	addListeners() {
-		this.handlerAnimate = this.animate.bind(this);
+    const el = this.webgl.renderer.domElement;
+    el.addEventListener("click", this.click.bind(this));
+  }
 
-		window.addEventListener('resize', this.resize.bind(this));
-		window.addEventListener('keyup', this.keyup.bind(this));
-		
-		const el = this.webgl.renderer.domElement;
-		el.addEventListener('click', this.click.bind(this));
-	}
+  animate() {
+    this.update();
+    this.draw();
 
-	animate() {
-		this.update();
-		this.draw();
+    this.raf = requestAnimationFrame(this.handlerAnimate);
+  }
 
-		this.raf = requestAnimationFrame(this.handlerAnimate);
-	}
+  // ---------------------------------------------------------------------------------------------
+  // PUBLIC
+  // ---------------------------------------------------------------------------------------------
 
-	// ---------------------------------------------------------------------------------------------
-	// PUBLIC
-	// ---------------------------------------------------------------------------------------------
+  update() {
+    if (this.gui.stats) this.gui.stats.begin();
+    if (this.webgl) this.webgl.update();
+    if (this.gui) this.gui.update();
+  }
 
-	update() {
-		if (this.gui.stats) this.gui.stats.begin();
-		if (this.webgl) this.webgl.update();
-		if (this.gui) this.gui.update();
-	}
+  draw() {
+    if (this.webgl) this.webgl.draw();
+    if (this.gui.stats) this.gui.stats.end();
+  }
 
-	draw() {
-		if (this.webgl) this.webgl.draw();
-		if (this.gui.stats) this.gui.stats.end();
-	}
+  // ---------------------------------------------------------------------------------------------
+  // EVENT HANDLERS
+  // ---------------------------------------------------------------------------------------------
 
-	// ---------------------------------------------------------------------------------------------
-	// EVENT HANDLERS
-	// ---------------------------------------------------------------------------------------------
+  resize() {
+    if (this.webgl) this.webgl.resize();
+  }
 
-	resize() {
-		if (this.webgl) this.webgl.resize();
-	}
+  keyup(e) {
+    // g
+    if (e.keyCode == 71) {
+      if (this.gui) this.gui.toggle();
+    }
+  }
 
-	keyup(e) {
-		// g
-		if (e.keyCode == 71) { if (this.gui) this.gui.toggle(); }
-	}
-
-	click(e) {
-		this.webgl.next();
-	}
+  click(e) {
+    this.webgl.next();
+  }
 }
--- src/scripts/webgl/WebGLView.js
+++ src/scripts/webgl/WebGLView.js
@@ -1,103 +1,107 @@
-import 'three';
-import { TweenLite } from 'gsap/TweenMax';
+import "three";
+import { TweenLite } from "gsap/TweenMax";
 
-import InteractiveControls from './controls/InteractiveControls';
-import Particles from './particles/Particles';
+import InteractiveControls from "./controls/InteractiveControls";
+import Particles from "./particles/Particles";
 
-const glslify = require('glslify');
+const glslify = require("glslify");
 
 export default class WebGLView {
+  constructor(app) {
+    this.app = app;
 
-	constructor(app) {
-		this.app = app;
+    this.samples = ["images/sample-06.png"];
 
-		this.samples = [
-			'images/sample-01.png',
-			'images/sample-02.png',
-			'images/sample-03.png',
-			'images/sample-04.png',
-			'images/sample-05.png',
-		];
+    this.initThree();
+    this.initParticles();
+    this.initControls();
 
-		this.initThree();
-		this.initParticles();
-		this.initControls();
+    const rnd = ~~(Math.random() * this.samples.length);
+    this.goto(rnd);
+  }
 
-		const rnd = ~~(Math.random() * this.samples.length);
-		this.goto(rnd);
-	}
+  initThree() {
+    // scene
+    this.scene = new THREE.Scene();
 
-	initThree() {
-		// scene
-		this.scene = new THREE.Scene();
+    // camera
+    this.camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      10000
+    );
+    this.camera.position.z = 300;
 
-		// camera
-		this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-		this.camera.position.z = 300;
+    // renderer
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
-		// renderer
-        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    // clock
+    this.clock = new THREE.Clock(true);
+  }
 
-        // clock
-		this.clock = new THREE.Clock(true);
-	}
+  initControls() {
+    this.interactive = new InteractiveControls(
+      this.camera,
+      this.renderer.domElement
+    );
+  }
 
-	initControls() {
-		this.interactive = new InteractiveControls(this.camera, this.renderer.domElement);
-	}
+  initParticles() {
+    this.particles = new Particles(this);
+    this.scene.add(this.particles.container);
+  }
 
-	initParticles() {
-		this.particles = new Particles(this);
-		this.scene.add(this.particles.container);
-	}
+  // ---------------------------------------------------------------------------------------------
+  // PUBLIC
+  // ---------------------------------------------------------------------------------------------
 
-	// ---------------------------------------------------------------------------------------------
-	// PUBLIC
-	// ---------------------------------------------------------------------------------------------
+  update() {
+    const delta = this.clock.getDelta();
 
-	update() {
-		const delta = this.clock.getDelta();
+    if (this.particles) this.particles.update(delta);
+  }
 
-		if (this.particles) this.particles.update(delta);
-	}
+  draw() {
+    this.renderer.render(this.scene, this.camera);
+  }
 
-	draw() {
-		this.renderer.render(this.scene, this.camera);
-	}
+  goto(index) {
+    // init next
+    if (this.currSample == null) this.particles.init(this.samples[index]);
+    // hide curr then init next
+    else {
+      this.particles.hide(true).then(() => {
+        this.particles.init(this.samples[index]);
+      });
+    }
 
+    this.currSample = index;
+  }
 
-	goto(index) {
-		// init next
-		if (this.currSample == null) this.particles.init(this.samples[index]);
-		// hide curr then init next
-		else {
-			this.particles.hide(true).then(() => {
-				this.particles.init(this.samples[index]);
-			});
-		}
+  next() {
+    if (this.currSample < this.samples.length - 1)
+      this.goto(this.currSample + 1);
+    else this.goto(0);
+  }
 
-		this.currSample = index;
-	}
+  // ---------------------------------------------------------------------------------------------
+  // EVENT HANDLERS
+  // ---------------------------------------------------------------------------------------------
 
-	next() {
-		if (this.currSample < this.samples.length - 1) this.goto(this.currSample + 1);
-		else this.goto(0);
-	}
+  resize() {
+    if (!this.renderer) return;
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
 
-	// ---------------------------------------------------------------------------------------------
-	// EVENT HANDLERS
-	// ---------------------------------------------------------------------------------------------
+    this.fovHeight =
+      2 *
+      Math.tan((this.camera.fov * Math.PI) / 180 / 2) *
+      this.camera.position.z;
 
-	resize() {
-		if (!this.renderer) return;
-		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-		this.fovHeight = 2 * Math.tan((this.camera.fov * Math.PI) / 180 / 2) * this.camera.position.z;
-
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-		if (this.interactive) this.interactive.resize();
-		if (this.particles) this.particles.resize();
-	}
+    if (this.interactive) this.interactive.resize();
+    if (this.particles) this.particles.resize();
+  }
 }
